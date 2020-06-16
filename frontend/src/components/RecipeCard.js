4b0aabf731f526faf2854eda8531390501c0ec46
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-06-15 21:24:56 -0600

    Flip meals cards

--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -1,15 +1,30 @@
-import React from "react"
+import React, { useState } from "react";
+import ReactCardFlip from 'react-card-flip';
+
 
 export default function RecipeCard ({recipe}) {
     const {strMeal, strInstructions, strYoutube, strMealThumb} = recipe
     // let x = Math.round(calories, 0.5)
-    console.log(recipe)
+    const [isFlipped, setIsFlipped] = useState(false);
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    }
+
     return(
-        <li className="recipe-card">
-            <button className="add-recipe" onClick={console.log("WTF")}>Add to favorite</button>
-            <h2>{strMeal}</h2>
-            <p>{strInstructions}</p>
-            <img className="recipe-card-image" src={strMealThumb} alt=""/>
-        </li>
+        <ReactCardFlip isFlipped={isFlipped} fliDirection="vertical">
+            <div>
+                <li className="front-recipe-card" onClick={handleClick}>
+                    <button className="add-recipe" onClick={console.log("WTF")}>Add to favorite</button>
+                    <h2>{strMeal}</h2>
+                    <img className="recipe-card-image" src={strMealThumb} alt=""/>
+                </li>
+            </div>
+            <div>
+                <li className="back-recipe-card" onClick={handleClick}>
+                    <p>{strInstructions}</p>
+                    <a href={strYoutube} target="_blank">Go to watch video</a>
+                </li>
+            </div>
+        </ReactCardFlip>
     )
 }
